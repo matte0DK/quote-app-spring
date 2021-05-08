0ac051f85933b639fb4229e0979919f585cde6cf
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-08 19:56:52 +0200

    Initial commit

---
+++ src/main/java/matteo/springframework/quoteappspring/QuoteAppSpringApplication.java
@@ -0,0 +1,13 @@
+package matteo.springframework.quoteappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matteo/springframework/quoteappspring/QuoteAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package matteo.springframework.quoteappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
